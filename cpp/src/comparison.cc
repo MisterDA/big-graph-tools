7f74b82e7ffbb377a28a8f922f6d7e9dca1fab50
Antonin Décimo <antonin.decimo@gmail.com>
2019-04-17 12:09:24 +0200

    Minimum waiting time, avg degree

--- cpp/src/comparison.cc
+++ cpp/src/comparison.cc
@@ -1,24 +1,45 @@
 #include "timetable.hh"
 
-// static void
-// min_waiting_time(timetable& ttbl, std::vector<timetable::T>& waiting_times)
-// {
-//     waiting_times.reserve(ttbl.stop_departures.size());
-//     for (size_t i = 0; i < ttbl.stop_departures.size(); ++i) {
-//         assert(ttbl.stop_departures[i].size() == ttbl.stop_arrivals[i].size());
-//         waiting_times[i] = std::numeric_limits<timetable::T>::max();
-//         for (size_t j = 0; j < ttbl.stop_departures[i].size() - 1; ++j) {
-//             auto t = ttbl.stop_arrivals[i][j + 1] - ttbl.stop_departures[i][j];
-//             waiting_times[i] = std::min(waiting_times[i], t);
-//         }
-//     }
-// }
-
 typedef int V;
 typedef mgraph<V, int> graph;
 
+static std::tuple<size_t, size_t, size_t>
+min_waiting_time(timetable& ttbl, std::vector<size_t>& waiting_times)
+{
+    size_t min = std::numeric_limits<size_t>::max(),
+        max = std::numeric_limits<size_t>::min(),
+        avg = 0, n = 0;
+
+    waiting_times.reserve(ttbl.stop_departures.size());
+    for (size_t s = 0; s < ttbl.stop_departures.size(); ++s) {
+        assert(ttbl.stop_departures[s].size() == ttbl.stop_arrivals[s].size());
+        waiting_times[s] = std::numeric_limits<timetable::T>::max();
+        for (size_t e = 0; e < ttbl.stop_departures[s].size() - 1; ++e) {
+            auto t = ttbl.stop_arrivals[s][e + 1] - ttbl.stop_departures[s][e];
+            auto m = std::min(waiting_times[s], (size_t)t);
+            waiting_times[s] = m;
+            min = std::min(min, m);
+            max = std::max(max, m);
+            avg += m;
+            ++n;
+        }
+    }
+    return std::make_tuple<>(min, avg / n, max);
+}
+
+static size_t
+avg_degree(const graph& graph)
+{
+    size_t avg = 0, n = 0;
+    for (const auto& u : graph) {
+        ++n;
+        avg += graph.degree(u);
+    }
+    return avg / n;
+}
+
 static graph *
-min_graph(timetable& ttbl, int ma = 30, int mb = 30) // TODO: transfers
+min_graph(timetable& ttbl, int ma = 30, int mb = 30)
 {
     std::vector<unit::graph::edge> edges;
     V max_vertex = 0;
@@ -71,15 +92,36 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30) // TODO: transfers
         ++max_vertex;
     }
 
+    // Is the transfer graph symmetric?
+    for (const auto& u : ttbl.transfers) {
+        for (const auto& e : ttbl.transfers[u]) {
+            edges.push_back(unit::graph::edge(stations_vertices[u],
+                                              stations_vertices[e.dst],
+                                              e.wgt));
+        }
+    }
+
     auto graph = new mgraph<V, int>(max_vertex, edges);
     return graph;
+}
+
+static void
+usage(void)
+{
+    std::cerr << "Usage: comparison <stop_times.csv> <transfers.csv>" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
     timetable ttbl(argv[1], argv[2]);
 
-    // std::vector<timetable::T> waiting_times;
-    // min_waiting_time(ttbl, waiting_times);
+    std::cout << "Minimum waiting time (secs): " << std::endl;
+    std::vector<size_t> waiting_times;
+    size_t min, max, avg;
+    std::tie(min, avg, max) = min_waiting_time(ttbl, waiting_times);
+    std::cout << "  {.min = " << min << ", .avg = " << avg << ", .max = "
+              << max << "}" << std::endl;
 
     graph *graph = min_graph(ttbl);
+    std::cout << "Average degree of minimum static graph: "
+              << avg_degree(*graph) << std::endl;
 }
