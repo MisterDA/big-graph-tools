d36b1f2fff7b8f050b39a42b7e419bb8815985ef
Antonin Décimo <antonin.decimo@gmail.com>
2019-04-18 10:45:59 +0200

    Fix minimum waiting time calc

--- cpp/src/comparison.cc
+++ cpp/src/comparison.cc
@@ -1,7 +1,9 @@
+#include <fstream>
+
 #include "timetable.hh"
 
 typedef int V;
-typedef mgraph<V, int> graph;
+typedef mgraph<V, int> graph_t;
 
 static std::tuple<size_t, size_t, size_t>
 min_waiting_time(timetable& ttbl, std::vector<size_t>& waiting_times)
@@ -14,8 +16,8 @@ min_waiting_time(timetable& ttbl, std::vector<size_t>& waiting_times)
     for (size_t s = 0; s < ttbl.stop_departures.size(); ++s) {
         assert(ttbl.stop_departures[s].size() == ttbl.stop_arrivals[s].size());
         waiting_times[s] = std::numeric_limits<timetable::T>::max();
-        for (size_t e = 0; e < ttbl.stop_departures[s].size() - 1; ++e) {
-            auto t = ttbl.stop_arrivals[s][e + 1] - ttbl.stop_departures[s][e];
+        for (size_t e = 0; e < ttbl.stop_departures[s].size(); ++e) {
+            auto t = ttbl.stop_departures[s][e] - ttbl.stop_arrivals[s][e];
             auto m = std::min(waiting_times[s], (size_t)t);
             waiting_times[s] = m;
             min = std::min(min, m);
@@ -27,23 +29,48 @@ min_waiting_time(timetable& ttbl, std::vector<size_t>& waiting_times)
     return std::make_tuple<>(min, avg / n, max);
 }
 
-static size_t
-avg_degree(const graph& graph)
+static std::pair<size_t, size_t>
+avg_degree(const graph_t& graph)
 {
-    size_t avg = 0, n = 0;
+    size_t in = 0, out = 0, n = 0;
+    std::vector<int> in_degree;
     for (const auto& u : graph) {
         ++n;
-        avg += graph.degree(u);
+        out += graph.degree(u);
+        for (const auto& e : graph[u])
+            in_degree[e.dst] += 1;
     }
-    return avg / n;
+    in_degree.resize(n);
+    for (const auto& d : in_degree)
+        in += d;
+
+    return std::make_pair<>(in/n, out/n);
 }
 
-static graph *
+typedef std::map<timetable::S, std::pair<V, V>> stops_vertices_t;  // (arrival, departure)
+typedef std::map<timetable::ST, V> stations_vertices_t;
+typedef std::vector<timetable::S> vertices_stops_t;
+typedef std::vector<timetable::ST> vertices_stations_t;
+
+struct min_graph_t {
+    graph_t *graph;
+    stops_vertices_t stops_vertices;
+    stations_vertices_t stations_vertices;
+    vertices_stops_t vertices_stops;
+    vertices_stations_t vertices_stations;
+    size_t stations_offset;
+};
+
+static struct min_graph_t
 min_graph(timetable& ttbl, int ma = 30, int mb = 30)
 {
     std::vector<unit::graph::edge> edges;
     V max_vertex = 0;
-    std::map<timetable::S, std::pair<V, V>> vertices; // (arrival, departure)
+    stops_vertices_t stops_vertices;
+    stations_vertices_t stations_vertices;
+    vertices_stops_t vertices_stops;
+    vertices_stations_t vertices_stations;
+    size_t stations_offset;
 
     for (size_t r = 0; r < ttbl.trips_of.size(); ++r) {
         const auto& route = ttbl.trips_of[r];
@@ -56,36 +83,40 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30)
                 auto dep_stop_id = ttbl.route_stops[r][s],
                     arr_stop_id = ttbl.route_stops[r][s + 1];
 
-                auto it = vertices.find(dep_stop_id);
-                if (it == vertices.end()) {
-                    vertices[dep_stop_id] = std::make_pair<>(max_vertex,
+                auto it = stops_vertices.find(dep_stop_id);
+                if (it == stops_vertices.end()) {
+                    stops_vertices[dep_stop_id] = std::make_pair<>(max_vertex,
                                                              max_vertex+1);
+                    vertices_stops[max_vertex] = dep_stop_id;
+                    vertices_stops[max_vertex + 1] = dep_stop_id;
                     max_vertex += 2;
                 }
-                it = vertices.find(arr_stop_id);
-                if (it == vertices.end()) {
-                    vertices[arr_stop_id] = std::make_pair<>(max_vertex,
+                it = stops_vertices.find(arr_stop_id);
+                if (it == stops_vertices.end()) {
+                    stops_vertices[arr_stop_id] = std::make_pair<>(max_vertex,
                                                              max_vertex+1);
+                    vertices_stops[max_vertex] = arr_stop_id;
+                    vertices_stops[max_vertex + 1] = arr_stop_id;
                     max_vertex += 2;
                 }
 
-                edges.push_back(unit::graph::edge(vertices[dep_stop_id].first,
-                                                  vertices[dep_stop_id].second,
+                edges.push_back(unit::graph::edge(stops_vertices[dep_stop_id].first,
+                                                  stops_vertices[dep_stop_id].second,
                                                   w));
-                edges.push_back(unit::graph::edge(vertices[dep_stop_id].second,
-                                                  vertices[arr_stop_id].first,
+                edges.push_back(unit::graph::edge(stops_vertices[dep_stop_id].second,
+                                                  stops_vertices[arr_stop_id].first,
                                                   e));
             }
         }
     }
 
-    std::map<timetable::ST, V> stations_vertices;
+    stations_offset = max_vertex;
     for (size_t st = 0; st < ttbl.station_stops.size(); ++st) {
         const auto& station = ttbl.station_stops[st];
         stations_vertices[st] = max_vertex;
-
+        vertices_stations[max_vertex] = st;
         for (size_t s = 0; s < station.size(); ++s) {
-            const auto& stop = vertices[station[s]];
+            const auto& stop = stops_vertices[station[s]];
             edges.push_back(unit::graph::edge(stop.first, max_vertex, ma));
             edges.push_back(unit::graph::edge(max_vertex, stop.second, mb));
         }
@@ -102,8 +133,27 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30)
     }
 
     auto graph = new mgraph<V, int>(max_vertex, edges);
-    return graph;
+
+    return {graph, stops_vertices, stations_vertices,
+            vertices_stops, vertices_stations, stations_offset};
 }
+
+#if 0
+static void
+graphviz(const graph& graph, const map_vertices_t& vertices,
+         const map_stations_vertices_t& stations_vertices,
+         const std::string& path)
+{
+    std::ofstream dot;
+    dot.open(path);
+    dot << "digraph g {" << std::endl;
+    dot << "  rankdir=\"LR\";" << std::endl;
+    for (const auto& u : graph) {
+        dot
+    }
+
+}
+#endif
 
 static void
 usage(void)
@@ -121,7 +171,7 @@ int main(int argc, char *argv[]) {
     std::cout << "  {.min = " << min << ", .avg = " << avg << ", .max = "
               << max << "}" << std::endl;
 
-    graph *graph = min_graph(ttbl);
-    std::cout << "Average degree of minimum static graph: "
-              << avg_degree(*graph) << std::endl;
+    // graph *graph = min_graph(ttbl);
+    // std::cout << "Average degree of minimum static graph: "
+    //           << avg_degree(*graph) << std::endl;
 }
