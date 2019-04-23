fd633879c300cfa722c3954a3d308476a8f3010c
Antonin Décimo <antonin.decimo@gmail.com>
2019-04-23 10:17:03 +0200

    Comparison min graph

--- cpp/src/comparison.cc
+++ cpp/src/comparison.cc
@@ -1,9 +1,15 @@
 #include <fstream>
 
+#include "mgraph.hh"
 #include "timetable.hh"
+#include "traversal.hh"
+#include "pruned_landmark_labeling.hh"
 
 typedef int V;
-typedef mgraph<V, int> graph_t;
+typedef int64_t W;
+typedef mgraph<V, W> graph_t;
+typedef pruned_landmark_labeling<graph_t> pl_lab;
+typedef mgraph<int, pl_lab::hubinfo> graphL;
 
 static std::tuple<size_t, size_t, size_t>
 min_waiting_time(timetable& ttbl, std::vector<size_t>& waiting_times)
@@ -37,8 +43,11 @@ avg_degree(const graph_t& graph)
     for (const auto& u : graph) {
         ++n;
         out += graph.degree(u);
-        for (const auto& e : graph[u])
+        for (const auto& e : graph[u]) {
+            if (in_degree.size() < (unsigned long)e.dst)
+                in_degree.resize(e.dst);
             in_degree[e.dst] += 1;
+        }
     }
     in_degree.resize(n);
     for (const auto& d : in_degree)
@@ -58,19 +67,31 @@ struct min_graph_t {
     stations_vertices_t stations_vertices;
     vertices_stops_t vertices_stops;
     vertices_stations_t vertices_stations;
-    size_t stations_offset;
 };
+
+static void
+add_min_edge(std::map<std::pair<V, V>, W>& graph,
+             const V& src, const V& dst, const W& wgt)
+{
+    auto key = std::make_pair<>(src, dst);
+    auto git = graph.find(key);
+    if (git != graph.end()) {
+        graph[key] = std::min(graph[key], wgt);
+    } else {
+        graph[key] = wgt;
+    }
+}
 
 static struct min_graph_t
 min_graph(timetable& ttbl, int ma = 30, int mb = 30)
 {
-    std::vector<unit::graph::edge> edges;
     V max_vertex = 0;
     stops_vertices_t stops_vertices;
     stations_vertices_t stations_vertices;
     vertices_stops_t vertices_stops;
     vertices_stations_t vertices_stations;
-    size_t stations_offset;
+
+    std::map<std::pair<V, V>, W> graph;
 
     for (size_t r = 0; r < ttbl.trips_of.size(); ++r) {
         const auto& route = ttbl.trips_of[r];
@@ -78,7 +99,7 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30)
             const auto& trip = route[t];
             for (size_t s = 0; s < trip.size() - 1; ++s) {
                 const auto& stop = trip[s];
-                auto w = stop.first - stop.second;
+                auto w = stop.second - stop.first;
                 auto e = trip[s+1].first - stop.second;
                 auto dep_stop_id = ttbl.route_stops[r][s],
                     arr_stop_id = ttbl.route_stops[r][s + 1];
@@ -87,38 +108,45 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30)
                 if (it == stops_vertices.end()) {
                     stops_vertices[dep_stop_id] = std::make_pair<>(max_vertex,
                                                              max_vertex+1);
-                    vertices_stops[max_vertex] = dep_stop_id;
-                    vertices_stops[max_vertex + 1] = dep_stop_id;
+                    vertices_stops.push_back(dep_stop_id);
+                    vertices_stops.push_back(dep_stop_id);
                     max_vertex += 2;
                 }
                 it = stops_vertices.find(arr_stop_id);
                 if (it == stops_vertices.end()) {
                     stops_vertices[arr_stop_id] = std::make_pair<>(max_vertex,
                                                              max_vertex+1);
-                    vertices_stops[max_vertex] = arr_stop_id;
-                    vertices_stops[max_vertex + 1] = arr_stop_id;
+                    vertices_stops.push_back(arr_stop_id);
+                    vertices_stops.push_back(arr_stop_id);
                     max_vertex += 2;
                 }
 
-                edges.push_back(unit::graph::edge(stops_vertices[dep_stop_id].first,
-                                                  stops_vertices[dep_stop_id].second,
-                                                  w));
-                edges.push_back(unit::graph::edge(stops_vertices[dep_stop_id].second,
-                                                  stops_vertices[arr_stop_id].first,
-                                                  e));
+                add_min_edge(graph,
+                             stops_vertices[dep_stop_id].first,
+                             stops_vertices[dep_stop_id].second,
+                             w);
+
+                add_min_edge(graph,
+                             stops_vertices[dep_stop_id].second,
+                             stops_vertices[arr_stop_id].first,
+                             e);
             }
+            auto end_stop = ttbl.route_stops[r][trip.size() - 1];
+            add_min_edge(graph,
+                         stops_vertices[end_stop].first,
+                         stops_vertices[end_stop].second,
+                         trip.back().second - trip.back().first);
         }
     }
 
-    stations_offset = max_vertex;
     for (size_t st = 0; st < ttbl.station_stops.size(); ++st) {
         const auto& station = ttbl.station_stops[st];
         stations_vertices[st] = max_vertex;
-        vertices_stations[max_vertex] = st;
+        vertices_stations.push_back(st);
         for (size_t s = 0; s < station.size(); ++s) {
             const auto& stop = stops_vertices[station[s]];
-            edges.push_back(unit::graph::edge(stop.first, max_vertex, ma));
-            edges.push_back(unit::graph::edge(max_vertex, stop.second, mb));
+            add_min_edge(graph, stop.first, max_vertex, ma);
+            add_min_edge(graph, max_vertex, stop.second, mb);
         }
         ++max_vertex;
     }
@@ -126,52 +154,115 @@ min_graph(timetable& ttbl, int ma = 30, int mb = 30)
     // Is the transfer graph symmetric?
     for (const auto& u : ttbl.transfers) {
         for (const auto& e : ttbl.transfers[u]) {
-            edges.push_back(unit::graph::edge(stations_vertices[u],
-                                              stations_vertices[e.dst],
-                                              e.wgt));
+            if (stations_vertices[u] != stations_vertices[e.dst])
+                add_min_edge(graph,
+                             stations_vertices[u],
+                             stations_vertices[e.dst],
+                             e.wgt);
         }
     }
 
-    auto graph = new mgraph<V, int>(max_vertex, edges);
 
-    return {graph, stops_vertices, stations_vertices,
-            vertices_stops, vertices_stations, stations_offset};
+    std::vector<unit::graph::edge> edges;
+    for (const auto& e : graph) {
+        edges.push_back(unit::graph::edge(e.first.first,
+                                          e.first.second,
+                                          e.second));
+    }
+    auto mg = new mgraph<V, int>(max_vertex, edges);
+
+    return {mg, stops_vertices, stations_vertices,
+            vertices_stops, vertices_stations};
 }
 
-#if 0
 static void
-graphviz(const graph& graph, const map_vertices_t& vertices,
-         const map_stations_vertices_t& stations_vertices,
+graphviz(const min_graph_t& graph,
          const std::string& path)
 {
     std::ofstream dot;
     dot.open(path);
     dot << "digraph g {" << std::endl;
     dot << "  rankdir=\"LR\";" << std::endl;
-    for (const auto& u : graph) {
-        dot
+    for (const auto& u : *graph.graph) {
+        if (u < graph.vertices_stops.size())
+            dot << "  " << u << ";" << std::endl;
+        else
+            dot << "  " << u << "[shape=point];" << std::endl;
+        for (const auto& e : (*graph.graph)[u])
+            dot << "  " << u << " -> " << e.dst
+                << " [label=\"" << e.wgt << "\"];" << std::endl;
     }
-
+    dot << "}" << std::endl;
+    dot.close();
 }
-#endif
 
 static void
-usage(void)
+graph_output(const mgraph<int, int>& graph, const std::string& path)
+{
+    std::ofstream gr;
+    gr.open(path);
+    for (auto u : graph) {
+        for (auto e : graph[u]) {
+            gr << u << " " << e.dst << " " << e.wgt << std::endl;
+        }
+    }
+    gr.close();
+}
+
+
+static void
+usage_exit(void)
 {
     std::cerr << "Usage: comparison <stop_times.csv> <transfers.csv>" << std::endl;
+    exit(1);
+}
+
+static void
+timeprofile(std::vector<std::pair<int, int>>& profile, const min_graph_t& mg,
+            const timetable& ttbl, const pl_lab& hl,
+            const V src, const V dst, const int departure_time)
+{
+    // coming soon
 }
 
 int main(int argc, char *argv[]) {
     timetable ttbl(argv[1], argv[2]);
+    min_graph_t mg = min_graph(ttbl);
 
-    std::cout << "Minimum waiting time (secs): " << std::endl;
-    std::vector<size_t> waiting_times;
-    size_t min, max, avg;
-    std::tie(min, avg, max) = min_waiting_time(ttbl, waiting_times);
-    std::cout << "  {.min = " << min << ", .avg = " << avg << ", .max = "
-              << max << "}" << std::endl;
+    std::vector<bool> is_sel(mg.graph->n(), true);
+    std::vector<pl_lab::edgeL> edg;
+    pl_lab hl(*mg.graph);
+    edg = hl.in_hub_edges(is_sel, is_sel);
+    auto queries =
+        read_csv(std::string(argv[3]), 6, "source","destination",
+                 "departure_time","log2_of_station_rank","station_rank","walk_time");
+    for (const auto& query : queries) {
+        V src = std::stoi(query[0]), dst = std::stoi(query[1]);
+        int departure_time = std::stoi(query[2]);
+        auto src_station = mg.stations_vertices[src],
+            dst_station = mg.stations_vertices[dst];
+    }
 
-    // graph *graph = min_graph(ttbl);
+
+        // graphviz(mg, "test.gv");
+
+
+
+//
+
+
+    // std::cout << "Minimum waiting time (secs): " << std::endl;
+    // std::vector<size_t> waiting_times;
+    // size_t min, max, avg;
+    // std::tie(min, avg, max) = min_waiting_time(ttbl, waiting_times);
+    // std::cout << "  {.min = " << min << ", .avg = " << avg << ", .max = "
+    //           << max << "}" << std::endl;
+
+
+
+    // size_t in_degree, out_degree;
+    // std::tie(in_degree, out_degree) = avg_degree(*mg.graph);
     // std::cout << "Average degree of minimum static graph: "
-    //           << avg_degree(*graph) << std::endl;
+    //           << in_degree << ", " << out_degree << std::endl;
+
 }
